fb5293a3ed351910f42450c6768960f5f5122a89
eddiekeem <cm730712@gmail.com>
2023-12-19 13:50:52 +0900

    dice 기록 구현

--- client/main.js
+++ client/main.js
@@ -1,5 +1,67 @@
-console.log(navigator.language);
+import { diceAnimation, getNode, getNodes, insertLast } from './lib/index.js';
 
-const a = 10;
+const [rollingButton, recordButton, resetButton] = getNodes(
+  '.buttonGroup > button'
+);
+const recordListWrapper = getNode('.recordListWrapper');
 
-const b = 'hello';
+// [phase-1]
+// 1. 주사위 굴리기
+// 2. 주사위 굴리기 버튼을 클릭하면 diceAnimation() 실행될 수 있도록
+
+// toggle
+// 1. 주사위 눈 값 가져오기
+// 2. 태그 만들기
+// 3. 뿌려주기
+let count = 0;
+let total = 0;
+
+const createItem = (value) => {
+  const template = /* html*/ `
+              <tr>
+            <td>${++count}</td>
+            <td>${value}</td>
+            <td>${(total += value)}</td>
+          </tr>
+  `;
+  return template;
+};
+
+function renderRecordItem() {
+  const diceValue = Number(getNode('#cube').dataset.dice);
+
+  insertLast('.recordList tbody', createItem(diceValue));
+}
+
+const handleRollingDice = (() => {
+  let isClicked = false;
+  let stopAnimation;
+  return () => {
+    if (!isClicked) {
+      // 실행
+      stopAnimation = setInterval(diceAnimation, 100);
+      recordButton.disabled = true;
+      resetButton.disabled = true;
+    } else {
+      // 정지
+      clearInterval(stopAnimation);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
+    }
+    isClicked = !isClicked;
+  };
+})();
+
+function handleRecord() {
+  recordListWrapper.hidden = false;
+
+  renderRecordItem();
+}
+
+function handleReset() {
+  recordListWrapper.hidden = false;
+}
+
+rollingButton.addEventListener('click', handleRollingDice);
+recordButton.addEventListener('click', handleRecord);
+recordButton.addEventListener('click', handleReset);
